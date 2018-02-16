db248871a36d3317e429ff78ae43205fd52941aa
levon0101 <levon.ecs@gmail.com>
2018-02-16 20:15:18 +0400

    End lesson 15-2

--- FriendOrganizer.DataAccess/Migrations/Configuration.cs
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -2,6 +2,7 @@ namespace FriendOrganizer.DataAccess.Migrations
 {
     using FriendOrganizer.Model;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -39,6 +40,22 @@ namespace FriendOrganizer.DataAccess.Migrations
                pn => pn.Number,
                new FriendPhoneNumber { Number = "+374 55979969", FriendId = context.Friends.First().Id }
                );
+
+
+            //context.Meetings.AddOrUpdate(
+            //    m => m.Title,
+            //    new Meeting
+            //    {
+            //        Title = "Watching Soccer",
+            //        DateFrom = new DateTime(2018, 5, 26),
+            //        DateTo = new DateTime(2018, 5, 26),
+            //        Friends = new List<Friend>
+            //        {
+            //            context.Friends.Single(f=>f.FirstName =="Thomas" && f.LastName == "Huber"),
+            //            context.Friends.Single(f=>f.FirstName =="Levon" && f.LastName == "Mardanyan")
+            //        }
+            //    });
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
